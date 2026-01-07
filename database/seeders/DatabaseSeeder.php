06811cf7034cd0af5df463358812508db7e2d662
Hashim Jilani <hashimjilani@Hashims-Mac-mini.local>
2026-01-07 13:08:56 +0300

    Adjusting seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,11 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
             BannerSeeder::class,
             CategorySeeder::class,
             RolePermissionSeeder::class,
-            UserChatSeeder::class,
         ]);
     }
 }
